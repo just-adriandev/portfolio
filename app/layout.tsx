88d19ed80b9e232bb9501f89f580ab7e4d33ef59
Adrian Graça <adrianspk1@gmail.com>
2025-01-29 18:00:30 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
           <Navbar/>
           {children}
           <About/>  
